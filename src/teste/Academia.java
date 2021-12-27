70cf04db0e15b9ab5daaf586a0862d98afab70fb
matlencar <malencar292@gmail.com>
2021-12-27 15:14:30 -0300

    implementação de informação,endereço,esporte

--- src/teste/Academia.java
+++ src/teste/Academia.java
@@ -10,11 +10,27 @@ public class Academia {
 		Scanner entrada = new Scanner(System.in);
 		System.out.println("Cadastre um novo aluno!");
 		System.out.println("Insira o nome do aluno: ");
+		
 		//recebe o nome do usuário
 		String nome = entrada.nextLine();
 		System.out.println("Insira o número de matrícula do aluno: ");
+		
 		String matricula = entrada.nextLine();
-		Aluno a1 = new Aluno(nome, matricula);
+		//Aluno a1 = new Aluno(nome, matricula);
+		
+		System.out.println("Insira as informações do aluno: ");
+		String informacao = entrada.nextLine();
+		
+		System.out.println("Endereço: ");
+		String endereco = entrada.nextLine();
+		
+		
+		System.out.println("Esporte praticado: ");
+		String esporte = entrada.nextLine();
+		
+		//System.out.println("Informações atualizadas");
+		Aluno a1 = new Aluno(nome, matricula,informacao,endereco,esporte);
+		
 		//cria um arquivo com o nome do aluno no diretório alunos e retorna um arquivo
 		File aluno1 = FabricaArquivoTexto.criar(nome, "alunos");
 		a1.setFile(aluno1);
