446dfec3cf1f16fba40fc787fda7bf98e78d4e48
lucas-serra <lucasdserra1234@gmail.com>
2021-12-27 20:39:04 -0300

    ajustes finais

--- src/desafio/Main.java
+++ src/desafio/Main.java
@@ -4,12 +4,12 @@ import java.io.File;
 
 public class Main {
 	public static void main(String[] args) {
-		File arq = new File("D:\\PROJETOS\\desafio\\src\\desafio\\texto.txt");
-		File dir = new File("D:\\PROJETOS\\desafio\\src\\desafio");
+		File arq = new File("C:\\estudo\\atividade_grupo_semana2\\desafio-mjv-segunda-semana\\Desafio-MJV-Segunda-Semana\\src\\desafio\\texto.txt");
+		File dir = new File("C:\\\\estudo\\\\atividade_grupo_semana2\\\\desafio-mjv-segunda-semana\\\\Desafio-MJV-Segunda-Semana\\\\src\\\\desafio");
 		// Exibe um relatório do arquivo passado como parâmetro		
-		RelatorioArquivo.exibirRelatorio(arq);
+		//RelatorioArquivo.exibirRelatorio(arq);
 		//Modifica um arquivo passado como parâmetro (o arquivo é completamente substituído pela string passada)		
-		ModificaArquivoTexto.modificar(arq);
+		//ModificaArquivoTexto.modificar(arq);
 		//Cria ou acessa um arquivo com o nome do arquivo (1º parâmetro) no diretório passado no 2º parâmetro)
 		//Caso o dirtório já exista ele cria o arquivo no diretório existente
 		File aluno1 = FabricaArquivoTexto.criar("aluno1", "alunos");
